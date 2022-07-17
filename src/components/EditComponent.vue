83a6babe53164008b11a5d1394fecad5ba67e00b
Dischapongpoom <ko.panda@hotmail.com>
2022-07-17 23:46:06 +0700

    Update EditComponent.vue

--- src/components/EditComponent.vue
+++ src/components/EditComponent.vue
@@ -32,14 +32,14 @@
             }
         },
         created() {
-            let apiURL = `http://localhost:4000/api/edit-student/${this.$route.params.id}`;
+            let apiURL = `http://localhost:4000/api/${this.$route.params.id}`;
             axios.get(apiURL).then((res) =>{
                 this.student = res.data;
             })
         },
         methods: {
             handleUpdateForm(){
-                let apiURL = `http://localhost:4000/api/update-student/${this.$route.params.id}`;
+                let apiURL = `http://localhost:4000/api/${this.$route.params.id}`;
 
                 axios.put(apiURL, this.student).then((res) =>{
                     console.log(res);
